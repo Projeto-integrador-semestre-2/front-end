42d6d77b51fe28d543f3362100a8bd6501213fb8
Vinicius Oliveira de Deus nequiz <vinicius.nequiz@mastsolucoes.com>
2024-11-24 23:30:14 -0300

    check is working!

--- Js/listUser.js
+++ Js/listUser.js
@@ -21,7 +21,9 @@ async function searchAllUsers() {
                         <p>CPF: ${data[0][i].CPF}</p>
                         <hr />
                         <div class="buttons">
-                            <button class="btn">Relatório Semanal</button>
+                            <button class="btn" onclick="relatorioSemanal(${
+                                data[0][i].CPF
+                            })">Relatório Semanal</button>
                             <button class="btn">Relatório Geral</button>
                             <div>
                                 <button class="btn btn-warning">Editar</button>
@@ -42,6 +44,31 @@ function filtrarPorCPF(lista, cpf) {
     })
 }
 
+async function relatorioSemanal(cpf) {
+    const response = await fetch(
+        'http://localhost:3000/check/relatorio_semanal',
+        {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+            body: JSON.stringify({
+                cpf: cpf,
+            }),
+        }
+    )
+    const data = await response.json()
+    if (data.messsage) {
+        alert('Erro: ' + data.messsage)
+    } else {
+        let classificação = 'Iniciante'
+        if (data.hours >= 6 && data.hours <= 10) classificação = 'Intermediário'
+        if (data.hours >= 11 && data.hours <= 20) classificação = 'Avançado'
+        if (data.hours >= 20) classificação = 'Extremamente avançado'
+        alert(
+            `Quantidade de horas nesta semana: ${data.hours}:${data.minutes}:${data.seconds}\nClassificado como: ${classificação}`
+        )
+    }
+}
+
 const searchInput = document.querySelector('input[name="searchCliente"]')
 const searchBtn = document.querySelector('.searchBtn')
 
@@ -56,7 +83,6 @@ searchBtn.onclick = async () => {
     }
     listUserBox.innerHTML = ''
     const users = filtrarPorCPF(data[0], searchInput.value)
-    console.log(users)
     const actualDay = new Date()
     for (let i = 0; i < users.length; i++) {
         const dataNasc = new Date(users[i].DataNasc)
@@ -68,7 +94,9 @@ searchBtn.onclick = async () => {
                         <p>CPF: ${users[i].CPF}</p>
                         <hr />
                         <div class="buttons">
-                            <button class="btn">Relatório Semanal</button>
+                            <button class="btn" onclick="relatorioSemanal(${
+                                users[i].CPF
+                            })">Relatório Semanal</button>
                             <button class="btn">Relatório Geral</button>
                             <div>
                                 <button class="btn btn-warning">Editar</button>
