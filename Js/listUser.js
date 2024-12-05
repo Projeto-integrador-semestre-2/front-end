f1badbb54b82722d58db5a23c1a5f4434c742606
Vinicius Oliveira de Deus nequiz <vinicius.nequiz@mastsolucoes.com>
2024-12-05 18:56:48 -0300

    final

--- Js/listUser.js
+++ Js/listUser.js
@@ -2,32 +2,42 @@ const listUserBox = document.querySelector('.list-box')
 const inputSearchBox = document.querySelector('.input[name="searchCliente"]')
 
 async function searchAllUsers() {
-    const response = await fetch('http://localhost:3000/user/all', {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'GET',
-    })
+    const response = await fetch(
+        'http://localhost:3000/check/relatorio_completo',
+        {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'GET',
+        }
+    )
     const data = await response.json()
     if (data.messsage) {
         alert('Erro: ' + data.messsage)
     }
-    const actualDay = new Date()
     for (let i = 0; i < data[0].length; i++) {
-        const dataNasc = new Date(data[0][i].DataNasc)
+        let classificacao = 'Iniciante'
+        if (
+            data[0][i].total_hours_overall >= 6 &&
+            data[0][i].total_hours_overall <= 10
+        )
+            classificacao = 'Intermediário'
+        if (
+            data[0][i].total_hours_overall >= 11 &&
+            data[0][i].total_hours_overall <= 20
+        )
+            classificacao = 'Avançado'
+        if (data[0][i].total_hours_overall >= 20)
+            classificacao = 'Extremamente avançado'
         listUserBox.innerHTML += `<div class="item">
                         <p>Nome: ${data[0][i].name}</p>
-                        <p>idade: ${
-                            actualDay.getFullYear() - dataNasc.getFullYear()
-                        }</p>
                         <p>CPF: ${data[0][i].CPF}</p>
+                        <p>Horas na semana: ${data[0][i].total_hours_weekly}</p>
+                        <p>Horas totais: ${data[0][i].total_hours_overall}</p>
+                        <p>Classificação: ${classificacao}</p>
                         <hr />
                         <div class="buttons">
                         Detalhado
-                            <button class="btn" onclick="relatorioSemanal(${
-                                data[0][i].CPF
-                            })">Relatório Semanal</button>
-                            <button class="btn" onclick="relatorioGeral(${
-                                data[0][i].CPF
-                            })">Relatório Geral</button>
+                            <button class="btn" onclick="relatorioSemanal(${data[0][i].CPF})">Relatório Semanal</button>
+                            <button class="btn" onclick="relatorioGeral(${data[0][i].CPF})">Relatório Geral</button>
                             <div>
                                 
                                
@@ -60,14 +70,14 @@ async function relatorioSemanal(cpf) {
     if (data.messsage) {
         alert('Erro: ' + data.messsage)
     } else {
-        let classificação = 'Iniciante'
-        if (data.hours >= 6 && data.hours <= 10) classificação = 'Intermediário'
-        if (data.hours >= 11 && data.hours <= 20) classificação = 'Avançado'
-        if (data.hours >= 20) classificação = 'Extremamente avançado'
+        let classificacao = 'Iniciante'
+        if (data.hours >= 6 && data.hours <= 10) classificacao = 'Intermediário'
+        if (data.hours >= 11 && data.hours <= 20) classificacao = 'Avançado'
+        if (data.hours >= 20) classificacao = 'Extremamente avançado'
         alert(
             `Quantidade de horas nesta semana: ${data.hours || '00'}:${
                 data.minutes || '00'
-            }:${data.seconds || '00'}\nClassificado como: ${classificação}`
+            }:${data.seconds || '00'}\nClassificado como: ${classificacao}`
         )
     }
 }
@@ -87,14 +97,14 @@ async function relatorioGeral(cpf) {
     if (data.messsage) {
         alert('Erro: ' + data.messsage)
     } else {
-        let classificação = 'Iniciante'
-        if (data.hours >= 6 && data.hours <= 10) classificação = 'Intermediário'
-        if (data.hours >= 11 && data.hours <= 20) classificação = 'Avançado'
-        if (data.hours >= 20) classificação = 'Extremamente avançado'
+        let classificacao = 'Iniciante'
+        if (data.hours >= 6 && data.hours <= 10) classificacao = 'Intermediário'
+        if (data.hours >= 11 && data.hours <= 20) classificacao = 'Avançado'
+        if (data.hours >= 20) classificacao = 'Extremamente avançado'
         alert(
             `Quantidade de horas nesta semana: ${data.hours || '00'}:${
                 data.minutes || '00'
-            }:${data.seconds || '00'}\nClassificado como: ${classificação}`
+            }:${data.seconds || '00'}\nClassificado como: ${classificacao}`
         )
     }
 }
@@ -103,10 +113,13 @@ const searchInput = document.querySelector('input[name="searchCliente"]')
 const searchBtn = document.querySelector('.searchBtn')
 
 searchBtn.onclick = async () => {
-    const response = await fetch('http://localhost:3000/user/all', {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'GET',
-    })
+    const response = await fetch(
+        'http://localhost:3000/check/relatorio_completo',
+        {
+            headers: { 'Content-Type': 'application/json' },
+            method: 'GET',
+        }
+    )
     const data = await response.json()
     if (data.messsage) {
         alert('Erro: ' + data.messsage)
@@ -115,23 +128,33 @@ searchBtn.onclick = async () => {
     const users = filtrarPorCPF(data[0], searchInput.value)
     const actualDay = new Date()
     for (let i = 0; i < users.length; i++) {
-        const dataNasc = new Date(users[i].DataNasc)
+        let classificacao = 'Iniciante'
+        if (
+            data[0][i].total_hours_overall >= 6 &&
+            data[0][i].total_hours_overall <= 10
+        )
+            classificacao = 'Intermediário'
+        if (
+            data[0][i].total_hours_overall >= 11 &&
+            data[0][i].total_hours_overall <= 20
+        )
+            classificacao = 'Avançado'
+        if (data[0][i].total_hours_overall >= 20)
+            classificacao = 'Extremamente avançado'
         listUserBox.innerHTML += `<div class="item">
-                        <p>Nome: ${users[i].name}</p>
-                        <p>idade: ${
-                            actualDay.getFullYear() - dataNasc.getFullYear()
-                        }</p>
-                        <p>CPF: ${users[i].CPF}</p>
+                        <p>Nome: ${data[0][i].name}</p>
+                        <p>CPF: ${data[0][i].CPF}</p>
+                        <p>Horas na semana: ${data[0][i].total_hours_weekly}</p>
+                        <p>Horas totais: ${data[0][i].total_hours_overall}</p>
+                        <p>Classificação: ${classificacao}</p>
                         <hr />
                         <div class="buttons">
-                            Detalhado
-                            <button class="btn" onclick="relatorioSemanal(${
-                                users[i].CPF
-                            })">Relatório Semanal</button>
-                            <button class="btn" onclick="relatorioGeral(${
-                                users[i].CPF
-                            })">Relatório Geral</button>
+                        Detalhado
+                            <button class="btn" onclick="relatorioSemanal(${data[0][i].CPF})">Relatório Semanal</button>
+                            <button class="btn" onclick="relatorioGeral(${data[0][i].CPF})">Relatório Geral</button>
                             <div>
+                                
+                               
                             </div>
                         </div>
                     </div>`
